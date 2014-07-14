38eaf84eecd40603c0d124b9bfe34f9fe4e28d73
Marcos Sorribas <marcos49x3@gmail.com>
2014-07-14 09:45:02 +0200

    Initial commit

---
+++ CartasAlFuturo/CartasAlFuturo/MSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MSAppDelegate.h
+//  CartasAlFuturo
+//
+//  Created by Marcos Sorribas Lopez on 14/07/14.
+//  Copyright (c) 2014 MarcosSorribas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
