1acab6f5170317278545ae37cf1f7d75b724d850
Marcos Sorribas <marcos49x3@gmail.com>
2014-07-14 12:08:55 +0200

    Creada la customView de la celda de cartas ‘Pending’ y añadido un titulo a la UITableView

---
+++ CartasAlFuturo/CartasAlFuturo/MSPendingLetterTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MSPendingLetterTableViewCell.h
+//  CartasAlFuturo
+//
+//  Created by Marcos Sorribas Lopez on 14/07/14.
+//  Copyright (c) 2014 MarcosSorribas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSPendingLetterTableViewCell : UITableViewCell
+
+@end
