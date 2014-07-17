f335be81a36c806e8e30c97c052bc0f21ae0bbf0
Marcos Sorribas <marcos49x3@gmail.com>
2014-07-17 13:10:12 +0200

    Implementando la UITableView de creación. - Ya consigo cambiar de tamaño dinámico

--- CartasAlFuturo/CartasAlFuturo/MSCreateLettersTableViewController.m
+++ CartasAlFuturo/CartasAlFuturo/MSCreateLettersTableViewController.m
@@ -8,22 +8,98 @@
 
 #import "MSCreateLettersTableViewController.h"
 
-@interface MSCreateLettersTableViewController ()
-
+@interface MSCreateLettersTableViewController ()<UITextFieldDelegate,UITableViewDelegate,UITableViewDataSource>
+@property (nonatomic) CGFloat titleRowHeight;
+@property (nonatomic) CGFloat dateRowHeight;
+@property (nonatomic) CGFloat contentRowHeight;
 @end
 
 @implementation MSCreateLettersTableViewController
 
+
+#pragma mark -
+#pragma mark - SetUp view methods
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
   
 }
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    switch (indexPath.section) {
+        case 0:
+            // Titulo
+            return self.titleRowHeight;
+            break;
+        case 1:
+            // Fecha
+            return self.dateRowHeight;
+            break;
+        case 2:
+            // Contenido
+            return self.contentRowHeight;
+            break;
+        default:
+            break;
+    }
+    return 0;
+}
+
+#pragma mark -
+#pragma mark - DataSource methods
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 3;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 1;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UITableViewCell *cell;
+    
+    switch (indexPath.section) {
+        case 0:
+            // Titulo
+            cell = [tableView dequeueReusableCellWithIdentifier:@"titleCell" forIndexPath:indexPath];
+            break;
+        case 1:
+            // Fecha
+            cell = [tableView dequeueReusableCellWithIdentifier:@"dateCell" forIndexPath:indexPath];
+            break;
+        case 2:
+            // Contenido
+            
+            cell = [tableView dequeueReusableCellWithIdentifier:@"contentCell" forIndexPath:indexPath];
+            break;
+        default:
+            break;
+    }
+    
+    return cell;
+}
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    switch (section) {
+        case 0:
+            return @"Titulo";
+        case 1:
+            return @"Fecha";
+        case 2:
+            return @"Contenido";
+        default:
+            return nil;
+    }
+}
+
+#pragma mark -
+#pragma mark - TextFieldDelegate methods
 
 
 #pragma mark -
 #pragma mark - Private methods
-
 
 
 #pragma mark -
@@ -32,4 +108,26 @@
 - (IBAction)doneTouched:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
+
+#pragma mark -
+#pragma mark - Getters & Setters
+-(CGFloat)titleRowHeight{
+    if (!_titleRowHeight) {
+        _titleRowHeight = 120;
+    }
+    return _titleRowHeight;
+}
+-(CGFloat)dateRowHeight{
+    if (!_dateRowHeight) {
+        _dateRowHeight = 44;
+    }
+    return _dateRowHeight;
+}
+-(CGFloat)contentRowHeight{
+    if (!_contentRowHeight) {
+        _contentRowHeight = 20;
+    }
+    return _contentRowHeight;
+}
+
 @end
