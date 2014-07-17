f76157caf93f1963448ba15c4e243bd8d7c63897
Marcos Sorribas <marcos49x3@gmail.com>
2014-07-17 16:51:29 +0200

    Primera aproximación a la primera entrada al crear una carta

--- CartasAlFuturo/CartasAlFuturo/MSCreateLettersTableViewController.m
+++ CartasAlFuturo/CartasAlFuturo/MSCreateLettersTableViewController.m
@@ -7,78 +7,74 @@
 //
 
 #import "MSCreateLettersTableViewController.h"
+#import "MSTitleTableViewCell.h"
 
-@interface MSCreateLettersTableViewController ()<UITextFieldDelegate,UITableViewDelegate,UITableViewDataSource>
+enum : NSUInteger {
+    TitleCell = 0,
+    DateCell = 1,
+    ContentCell = 2,
+    UnknowCell = -1
+};
+typedef NSInteger cellIdentificator;
+
+@interface MSCreateLettersTableViewController ()<UITableViewDelegate,UITableViewDataSource>
+
 @property (nonatomic) CGFloat titleRowHeight;
 @property (nonatomic) CGFloat dateRowHeight;
 @property (nonatomic) CGFloat contentRowHeight;
+
+@property (nonatomic) CGFloat titleSectionHeight;
+@property (nonatomic) CGFloat dateSectionHeight;
+@property (nonatomic) CGFloat contentSectionHeight;
+
+@property (nonatomic,strong) NSIndexPath *titlePath;
+@property (nonatomic,strong) NSIndexPath *datePath;
+@property (nonatomic,strong) NSIndexPath *contentPath;
+
+@property (nonatomic,strong) MSTitleTableViewCell *cell;
+
 @end
 
 @implementation MSCreateLettersTableViewController
 
-
 #pragma mark -
-#pragma mark - SetUp view methods
+#pragma mark - Constans
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    
-  
-}
+NSUInteger const numberOfSections = 3;
+NSUInteger const numberOfRowsInSection = 1;
 
--(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    switch (indexPath.section) {
-        case 0:
-            // Titulo
-            return self.titleRowHeight;
-            break;
-        case 1:
-            // Fecha
-            return self.dateRowHeight;
-            break;
-        case 2:
-            // Contenido
-            return self.contentRowHeight;
-            break;
-        default:
-            break;
-    }
-    return 0;
-}
+NSUInteger const titleSection = 0;
+NSUInteger const dateSection = 1;
+NSUInteger const contentSection = 2;
 
 #pragma mark -
 #pragma mark - DataSource methods
 
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
-    return 3;
+    return numberOfSections;
 }
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 1;
+    return numberOfRowsInSection;
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
     UITableViewCell *cell;
     
-    switch (indexPath.section) {
-        case 0:
-            // Titulo
+    switch ([self compareIndexPath:indexPath]) {
+        case TitleCell:
             cell = [tableView dequeueReusableCellWithIdentifier:@"titleCell" forIndexPath:indexPath];
+            self.cell = (MSTitleTableViewCell*)cell;
             break;
-        case 1:
-            // Fecha
+        case DateCell:
             cell = [tableView dequeueReusableCellWithIdentifier:@"dateCell" forIndexPath:indexPath];
             break;
-        case 2:
-            // Contenido
-            
+        case ContentCell:
             cell = [tableView dequeueReusableCellWithIdentifier:@"contentCell" forIndexPath:indexPath];
             break;
         default:
             break;
     }
-    
     return cell;
 }
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
@@ -95,11 +91,86 @@
 }
 
 #pragma mark -
-#pragma mark - TextFieldDelegate methods
+#pragma mark - TableViewDelegate methods
 
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [self.tableView beginUpdates];
+    
+    switch ([self compareIndexPath:indexPath]) {
+        case TitleCell:
+            [self.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+            break;
+        case DateCell:
+            
+            break;
+        case ContentCell:
+            
+            break;
+        default:
+            break;
+    }
+    [self.tableView endUpdates];
+    return indexPath;
+}
 
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    switch ([self compareIndexPath:indexPath]) {
+        case TitleCell:
+            return self.titleRowHeight;
+            break;
+        case DateCell:
+            return self.dateRowHeight;
+            break;
+        case ContentCell:
+            return self.contentRowHeight;
+            break;
+        default:
+            break;
+    }
+    return 0;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    switch (section) {
+        case titleSection:
+            return self.titleSectionHeight;
+            break;
+        case dateSection:
+            return self.dateSectionHeight;
+            break;
+        case contentSection:
+            return self.contentSectionHeight;
+            break;
+        default:
+            break;
+    }
+    return 0;
+}
 #pragma mark -
 #pragma mark - Private methods
+
+-(cellIdentificator)compareIndexPath:(NSIndexPath*)indexPath{
+    switch (indexPath.section) {
+        case 0:
+            if (indexPath.row == 0) {
+                return TitleCell;
+            }
+            break;
+        case 1:
+            if (indexPath.row == 0) {
+                return DateCell;
+            }
+            break;
+        case 2:
+            if (indexPath.row == 0) {
+                return ContentCell;
+            }
+            break;
+        default:
+            break;
+    }
+    return UnknowCell;
+}
 
 
 #pragma mark -
@@ -111,23 +182,64 @@
 
 #pragma mark -
 #pragma mark - Getters & Setters
+
+#warning Relativizar todos los numeros
+
 -(CGFloat)titleRowHeight{
     if (!_titleRowHeight) {
-        _titleRowHeight = 120;
+        _titleRowHeight = 155;
     }
     return _titleRowHeight;
 }
 -(CGFloat)dateRowHeight{
     if (!_dateRowHeight) {
-        _dateRowHeight = 44;
+        _dateRowHeight = 0;
     }
     return _dateRowHeight;
 }
 -(CGFloat)contentRowHeight{
     if (!_contentRowHeight) {
-        _contentRowHeight = 20;
+        _contentRowHeight = 0;
     }
     return _contentRowHeight;
 }
 
+-(CGFloat)titleSectionHeight{
+    if (!_titleSectionHeight) {
+        _titleSectionHeight = 25;
+    }
+    return _titleSectionHeight;
+}
+
+-(CGFloat)dateSectionHeight{
+    if (!_dateSectionHeight) {
+        _dateSectionHeight = 55;
+    }
+    return _dateSectionHeight;
+}
+-(CGFloat)contentSectionHeight{
+    if (!_contentSectionHeight) {
+        _contentSectionHeight = 55;
+    }
+    return _contentSectionHeight;
+}
+
+-(NSIndexPath *)titlePath{
+    if (!_titlePath) {
+        _titlePath = [NSIndexPath indexPathForRow:0 inSection:1];
+    }
+    return _titlePath;
+}
+-(NSIndexPath *)datePath{
+    if (!_datePath) {
+        _datePath = [NSIndexPath indexPathForRow:0 inSection:2];
+    }
+   return _datePath;
+}
+-(NSIndexPath *)contentPath{
+    if (!_contentPath) {
+        _contentPath = [NSIndexPath indexPathForRow:0 inSection:3];
+    }
+    return _contentPath;
+}
 @end
---
+++ CartasAlFuturo/CartasAlFuturo/MSTitleTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MSTitleTableViewCell.h
+//  CartasAlFuturo
+//
+//  Created by Marcos Sorribas Lopez on 17/07/14.
+//  Copyright (c) 2014 MarcosSorribas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSTitleTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UITextField *myTextField;
+@end
