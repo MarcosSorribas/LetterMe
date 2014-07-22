b221a0187af8eca60e7a3bdb3641a9e94233ecf6
Marcos Sorribas <marcos49x3@gmail.com>
2014-07-22 11:21:02 +0200

    Tab bar un poco custom

---
+++ CartasAlFuturo/CartasAlFuturo/MSCustomTabBar.h
@@ -0,0 +1,13 @@
+//
+//  MSCustomTabBar.h
+//  CartasAlFuturo
+//
+//  Created by Marcos Sorribas Lopez on 22/07/14.
+//  Copyright (c) 2014 MarcosSorribas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSCustomTabBar : UITabBar
+
+@end
---
+++ CartasAlFuturo/CartasAlFuturo/MSCustomTabBar.m
@@ -0,0 +1,40 @@
+//
+//  MSCustomTabBar.m
+//  CartasAlFuturo
+//
+//  Created by Marcos Sorribas Lopez on 22/07/14.
+//  Copyright (c) 2014 MarcosSorribas. All rights reserved.
+//
+
+#import "MSCustomTabBar.h"
+
+@implementation MSCustomTabBar
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+
+
+-(void)awakeFromNib{
+    [[UITabBar appearance] setBarStyle:UIBarStyleBlack];
+    
+    [[UITabBar appearance] setSelectedImageTintColor:[UIColor colorWithRed:0.845 green:0.708 blue:0.671 alpha:1]];
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
