84763b5f4fe9b8b85ad41e7d2fda0077c3dd1f75
Marcos Sorribas <marcos49x3@gmail.com>
2014-09-05 18:55:50 +0200

    Local notifications implemented

--- CartasAlFuturo/CartasAlFuturo/MSAppDelegate.m
+++ CartasAlFuturo/CartasAlFuturo/MSAppDelegate.m
@@ -24,6 +24,9 @@
     [self customizeTabBarController:tabBarController.tabBar];
     [self prepareFirstControllerFrom:tabBarController];
     [self mailManStart];
+    
+    application.applicationIconBadgeNumber = 0;
+    
     return YES;
 }
 
@@ -48,6 +51,7 @@
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     [self mailManStart];
+    application.applicationIconBadgeNumber = 0;
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
--- CartasAlFuturo/CartasAlFuturo/MSCreateLetterViewController.m
+++ CartasAlFuturo/CartasAlFuturo/MSCreateLetterViewController.m
@@ -425,6 +425,17 @@ NSInteger const navigationBarheight = 64;
     letter.letterContent = self.letterContent;
     letter.letterOpenDate = self.letterOpenDate;
     [self.manageDocument.managedObjectContext.undoManager endUndoGrouping];
+    [self createLocalNotification:letter.letterOpenDate];
+}
+
+-(void)createLocalNotification:(NSDate*)openDate{
+    UILocalNotification *notification = [[UILocalNotification alloc] init];
+    
+    notification.fireDate = openDate;
+    notification.alertBody = @"¡Te acaba de llegar una carta del pasado! Entra para leerla ahora mismo.";
+    notification.applicationIconBadgeNumber = [[UIApplication sharedApplication] applicationIconBadgeNumber] + 1;
+
+    [[UIApplication sharedApplication] scheduleLocalNotification:notification];
 }
 
 -(void)animateMistakeIn:(UIView*)view{
